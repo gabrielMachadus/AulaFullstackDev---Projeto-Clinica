3d07770ba89a3bca8d4f9ab17a619c9eaef0b7c4
Machado, Gabriel <gabrielgamerlive@gmail.com>
2023-08-09 23:32:21 -0300

    arquivos novos projeto clinica

--- src/main/java/util/ApiCep.java
+++ src/main/java/util/ApiCep.java
@@ -20,37 +20,18 @@ public class ApiCep {
         if(cep.length() == 10 ){
             try {
                 cep = (cep.replace(".","").replace("-",""));
-
                 this.url_api = "https://viacep.com.br/ws/"+cep+"/json/";
-
-                // mostra url da API
-                // System.out.println(url_api);
-
-                //Cria o client de acesso
                 HttpClient client = HttpClient.newHttpClient();
-
-                //faz a requisição
                 HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url_api)).build();
-
-                //devolve a resposta da requisição
                 HttpResponse<String> response = client.send(request,HttpResponse.BodyHandlers.ofString());
-                //se a requisição retornar e encontrar da sucesso
-                //System.out.println(response.statusCode());
                 if(response.statusCode()==200){
                     if(response.body().contains("erro")){
                      System.out.println("Cep não encontrado ou com erro!");
                     }else{
-                        //cria uma nova instancia do json
-                        Gson gson = new Gson();
-                        //converte o  Json em objeto conforme os atributos existentes na classe
-                        cep_preenchido = gson.fromJson(response.body(),Cep.class);
-                        // System.out.println("Dados da API JSON:"+response.body().toString());
-                        //System.out.println("Dados convertidos no Objeto: "+cep_preenchido.toString());
+                       Gson gson = new Gson();
+                       cep_preenchido = gson.fromJson(response.body(),Cep.class);
                     }
-
                 }
-
-
             }catch (Exception e) {
                 System.out.println("Erro: " + e.getMessage() + "\n" + e.getLocalizedMessage());
             }
@@ -58,7 +39,5 @@ public class ApiCep {
         }else{
             System.out.println("Cep no formato incorreto ou inválido!");
         }
-
     }
-
 }
